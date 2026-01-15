0782271e54ee104228094f9718b33318aed5b6d5
Manish Kumar <manishkumarpharma68@gmail.com>
2026-01-15 11:29:33 +0530

    Added first beginner pharmacy Python program

---
+++ drug_list.py
@@ -0,0 +1,8 @@
+# Beginner Python Program for Pharmacy
+# Author: Manish Kumar
+
+drugs = ["Paracetamol", "Ibuprofen", "Amoxicillin"]
+
+print("Common Drugs List:")
+for drug in drugs:
+    print(drug)
